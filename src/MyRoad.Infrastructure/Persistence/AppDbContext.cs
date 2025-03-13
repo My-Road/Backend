abc061581f694286abf5388174fb49f38d56c6db
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-03-13 16:21:21 +0200

    "refactor(db): migrate AppDbContext to support identity-based authentication"

--- src/MyRoad.Infrastructure/Persistence/AppDbContext.cs
+++ src/MyRoad.Infrastructure/Persistence/AppDbContext.cs
@@ -1,13 +1,16 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using MyRoad.Domain.Entities.Users;
+using MyRoad.Domain.Users;
+using MyRoad.Infrastructure.Identity.Entities;
 using MyRoad.Infrastructure.Persistence.config;
 
 namespace MyRoad.Infrastructure.Persistence;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>(options)
 {
-    public DbSet<User> Users { get; set; }
+    public  DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
