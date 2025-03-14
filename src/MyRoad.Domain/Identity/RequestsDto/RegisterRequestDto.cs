8f7cfca522404e65e40511f79ab523fb880e588a
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-03-14 14:55:51 +0200

    add Role entity to Register RequestDto

--- src/MyRoad.Domain/Identity/RequestsDto/RegisterRequestDto.cs
+++ src/MyRoad.Domain/Identity/RequestsDto/RegisterRequestDto.cs
@@ -6,4 +6,6 @@ public class RegisterRequestDto
     public string Password { get; set; }
     public string ConfirmPassword { get; set; }
     public string Email { get; set; }
+    public string Role { get; set; }
+    
 }
