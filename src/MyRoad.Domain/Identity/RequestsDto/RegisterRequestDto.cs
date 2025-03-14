8ce617c258b40867b61f631db3bd84852aeeff13
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-03-14 14:29:12 +0200

    add RegisterRequestDto

---
+++ src/MyRoad.Domain/Identity/RequestsDto/RegisterRequestDto.cs
@@ -0,0 +1,9 @@
+namespace MyRoad.Domain.Identity.RequestsDto;
+
+public class RegisterRequestDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+    public string Email { get; set; }
+}
