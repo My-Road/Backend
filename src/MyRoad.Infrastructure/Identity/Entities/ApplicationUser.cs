6bf4f7599029afff56eff434a0e83a0da3f1d658
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-03-16 16:59:16 +0200

    Removed the redundant Email property since it's already inherited from IdentityUse.

--- src/MyRoad.Infrastructure/Identity/Entities/ApplicationUser.cs
+++ src/MyRoad.Infrastructure/Identity/Entities/ApplicationUser.cs
@@ -4,10 +4,9 @@ namespace MyRoad.Infrastructure.Identity.Entities;
 
 public class ApplicationUser : IdentityUser<long>
 {
-    public string Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Role { get; set; }
     public bool IsActive { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
 }
