f35b3e74f4c33b899bb52ead46d6fba6a0aa6647
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-03-16 17:40:00 +0200

    edit RegisterValidator

--- src/MyRoad.Domain/Identity/Validators/RegisterValidator.cs
+++ src/MyRoad.Domain/Identity/Validators/RegisterValidator.cs
@@ -8,28 +8,21 @@ public class RegisterValidator : AbstractValidator<RegisterRequestDto>
 {
     public RegisterValidator()
     {
-        RuleFor(x => x.Username)
-            .NotEmpty()
-            .WithMessage("Username is required")
-            .MaximumLength(10);
         RuleFor(x => x.Email)
-            .NotEmpty()
-            .WithMessage("Email is required")
-            .EmailAddress();
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email address");
+
         RuleFor(x => x.Role)
             .NotEmpty()
             .Must(x => x is "Admin" or "Manager")
-            .WithMessage("Role is Admin or Manager");
+            .WithMessage("Role must be either Admin or Manager");
 
-        RuleFor(x => x.Password)
-            .NotEmpty()
-            .WithMessage("Password is required")
-            .MinimumLength(8);
-        RuleFor(x => x.ConfirmPassword)
-            .NotEmpty()
-            .WithMessage("ConfirmPassword is required")
-            .Equal(y => y.ConfirmPassword)
-            .WithMessage("ConfirmPassword does not match");
-
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required");
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required");
+        RuleFor(x => x.PhoneNumber)
+            .MaximumLength(14);
+        
     }
 }
