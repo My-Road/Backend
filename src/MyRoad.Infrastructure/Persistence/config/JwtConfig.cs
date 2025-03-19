2c83ea4043110a2357a396ef6eb18f0be8afedd8
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-03-19 17:40:11 +0200

    Initialize JwtConfig string properties with default values

--- src/MyRoad.Infrastructure/Persistence/config/JwtConfig.cs
+++ src/MyRoad.Infrastructure/Persistence/config/JwtConfig.cs
@@ -2,8 +2,8 @@ namespace MyRoad.Infrastructure.Persistence.config;
 
 public class JwtConfig
 {
-    public string Key { get; set; }
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
+    public string Key { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
     public double DurationInDays { get; set; }
 }
