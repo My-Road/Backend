73ec82eb284e3c496a93f8dd7c788970ce1d4c52
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-04-07 13:48:33 +0200

    Implement validation rules with CascadeMode.Stop to show only the first error

--- src/MyRoad.Domain/Identity/Validators/ForgotPasswordValidator.cs
+++ src/MyRoad.Domain/Identity/Validators/ForgotPasswordValidator.cs
@@ -7,10 +7,12 @@ public class ForgotPasswordValidator : AbstractValidator<ForgetPasswordRequestDt
     public ForgotPasswordValidator()
     {
         RuleFor(x => x.NewPassword)
+            .Cascade(CascadeMode.Stop)
             .NotEmpty().WithMessage("Current password is required")
             .Equal(u => u.ConfirmNewPassword)
             .WithMessage("Current passwords does not match");
         RuleFor(x => x.NewPassword)
+            .Cascade(CascadeMode.Stop)
             .NotEmpty().WithMessage("New password is required")
             .MinimumLength(8).WithMessage("New password must be at least 8 characters")
             .Matches(@"[A-Z]").WithMessage("New password must contain at least one uppercase letter")
