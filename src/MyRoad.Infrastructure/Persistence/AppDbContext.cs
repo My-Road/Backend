6e3315a41381b139127f933f23cfffe7d025a7dd
Obada <obada.awwadh9@gmail.com>
2025-04-08 16:27:14 +0200

    DP-26 Create Workers Table

--- src/MyRoad.Infrastructure/Persistence/AppDbContext.cs
+++ src/MyRoad.Infrastructure/Persistence/AppDbContext.cs
@@ -5,12 +5,14 @@ using MyRoad.Domain.Identity.Enums;
 using MyRoad.Domain.Users;
 using MyRoad.Infrastructure.Identity.Entities;
 using MyRoad.Infrastructure.Persistence.config;
+using MyRoad.Infrastructure.Wokrers;
 
 namespace MyRoad.Infrastructure.Persistence;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options)
     : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>(options)
 {
+    public DbSet<Workers> Workers { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
@@ -19,5 +21,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options)
             .HasConversion<string>();
         modelBuilder.ApplyConfiguration(new UserConfiguration());
         modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        modelBuilder.ApplyConfiguration(new WorkersConfiguration());
+
     }
 }
