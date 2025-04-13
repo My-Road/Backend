33c07ff88eacd9144a5fb4c2a78a7dfbd5360b2d
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-04-13 18:06:53 +0300

    add relation one to many between employee and employee Payment

--- src/MyRoad.Domain/Payments/EmployeePayment.cs
+++ src/MyRoad.Domain/Payments/EmployeePayment.cs
@@ -1,8 +1,10 @@
+using MyRoad.Domain.Employees;
+
 namespace MyRoad.Domain.Payments;
 
 public class EmployeePayment : Payment
 {
     public long EmployeeId { get; set; }
-    // public required  Employee Employee{ get; set; }
+    public required Employee Employee { get; set; }
  
 }
