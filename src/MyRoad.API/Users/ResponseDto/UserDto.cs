b0a98c81c2ba1f579afd90deb71506fe9370cdf2
Abdullmenem Fayez <abdullmenem2002@gmail.com>
2025-06-25 18:08:22 +0300

    add id

--- src/MyRoad.API/Users/ResponseDto/UserDto.cs
+++ src/MyRoad.API/Users/ResponseDto/UserDto.cs
@@ -4,6 +4,7 @@ namespace MyRoad.API.Users.ResponseDto;
 
 public class UserDto
 {
+    public long Id { get; set; }
     public string? Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
